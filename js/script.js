81f563ca1b34b16e13b51c3f9acb7c945649fb6c
nechiyan3 <navasnechiyan3@gmail.com>
2025-11-26 18:26:16 +0530

    Add initial landing page with statistics section and avatar grid

---
+++ js/script.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F97316",
+        "background-light": "#FFFFFF",
+        "background-dark": "#111827",
+        "custom-green": "#2F6253",
+        "custom-orange": "#E97315",
+      },
+      borderRadius: {
+        DEFAULT: "1rem",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
+  },
+};
